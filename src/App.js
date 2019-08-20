9ab0aaabc79b0665adbe98d6829fda3b203c7851
AqingCyan <xuekaiqi@creatshare.com>
2019-08-21 01:03:26 +0800

    ADD: 增加展示组件与容器组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import PriceList from "./components/PriceList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+  {
+    "id": 1,
+    "title": "去北京旅游",
+    "price": 200,
+    "date": "2019-08-21",
+    "category": {
+      "id": "1",
+      "name": "旅行",
+      "type": "outcome"
+    }
+  },
+  {
+    "id": 2,
+    "title": "去北京旅游",
+    "price": 300,
+    "date": "2019-08-21",
+    "category": {
+      "id": "1",
+      "name": "旅行",
+      "type": "outcome"
+    }
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <PriceList
+          items={items}
+          onModifyItem={(item) => {alert(item.id)}}
+          onDeleteItem={(item) => {alert(item.id)}}/>
+      </div>
+    )
+  }
 }
 
 export default App;
