e56a1d901541dfbb4b7c8742ff42cef89a96c8ab
AqingCyan <xuekaiqi@creatshare.com>
2019-08-21 23:38:49 +0800

    ADD: 完成Tab切换组件

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import React, {Component} from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import PriceList from "./components/PriceList";
+import React, { Component } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import PriceList from "./components/PriceList"
+import ViewTab from "./components/ViewTab"
+import { LIST_VIEW, CHART_VIEW } from "./util/utility"
 
 const items = [
   {
-    "id": 1,
-    "title": "去北京旅游",
-    "price": 200,
-    "date": "2019-08-21",
-    "category": {
-      "id": "1",
-      "name": "旅行",
-      "type": "outcome"
+    id: 1,
+    title: "去北京旅游",
+    price: 200,
+    date: "2019-08-21",
+    category: {
+      id: "1",
+      name: "旅行",
+      type: "outcome",
+      iconName: "ios-plane"
     }
   },
   {
-    "id": 2,
-    "title": "去北京旅游",
-    "price": 300,
-    "date": "2019-08-21",
-    "category": {
-      "id": "1",
-      "name": "旅行",
-      "type": "outcome"
+    id: 2,
+    title: "去北京旅游",
+    price: 300,
+    date: "2019-08-21",
+    category: {
+      id: "1",
+      name: "旅行",
+      type: "outcome",
+      iconName: "ios-plane"
     }
   }
-];
+]
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <PriceList
-          items={items}
-          onModifyItem={(item) => {alert(item.id)}}
-          onDeleteItem={(item) => {alert(item.id)}}/>
+        <ViewTab
+          activeTab={LIST_VIEW}
+          onTabChange={view => {
+            console.log(view)
+          }}
+        />
       </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -1,25 +1,74 @@
-import React from "react";
+import React from "react"
+import Ionicon from "react-ionicons"
+import PropTypes from "prop-types"
 
-const PriceList = ({items, onModifyItem, onDeleteItem}) => {
+const PriceList = ({ items, onModifyItem, onDeleteItem }) => {
   return (
     <ul className="list-group list-group-flush">
-      {
-        items.map(item => (
-          <li className="list-group-item d-flex justify-content-between align-items-center" key={item.id}>
-            <span className="col-1 badge badge-primary">{item.category.name}</span>
-            <span className="col-5">{item.title}</span>
-            <span className="col-2 font-weight-bold">
-              {item.category.type === 'income' ? '+' : '-'}
-              {item.price}元
-            </span>
-            <span className="col-2">{item.date}</span>
-            <button className="col-1 btn btn-primary" onClick={() => {onModifyItem(item)}}>编辑</button>
-            <button className="col-1 btn btn-danger" onClick={() => {onDeleteItem(item)}}>删除</button>
-          </li>
-        ))
-      }
+      {items.map(item => (
+        <li
+          className="list-group-item d-flex justify-content-between align-items-center"
+          key={item.id}
+        >
+          <span className="col-1">
+            <Ionicon
+              className="rounded-circle"
+              fontSize="30px"
+              style={{ backgroundColor: "#007bff", padding: "5px" }}
+              color={"#ffffff"}
+              icon={item.category.iconName}
+            />
+          </span>
+          <span className="col-5">{item.title}</span>
+          <span className="col-2 font-weight-bold">
+            {item.category.type === "income" ? "+" : "-"}
+            {item.price}元
+          </span>
+          <span className="col-2">{item.date}</span>
+          <span
+            className="col-1"
+            // 这样写可以绑定 this 到组件
+            onClick={() => {
+              onModifyItem(item)
+            }}
+          >
+            <Ionicon
+              className="rounded-circle"
+              fontSize="30px"
+              style={{ backgroundColor: "#28a754", padding: "5px" }}
+              color={"#ffffff"}
+              icon="ios-create-outline"
+            />
+          </span>
+          <span
+            className="col-1"
+            onClick={() => {
+              onDeleteItem(item)
+            }}
+          >
+            <Ionicon
+              className="rounded-circle"
+              fontSize="30px"
+              style={{ backgroundColor: "#dc3545", padding: "5px" }}
+              color={"#ffffff"}
+              icon="ios-close"
+            />
+          </span>
+        </li>
+      ))}
     </ul>
   )
-};
+}
 
-export default PriceList;
+// props 类型检查
+PriceList.propTypes = {
+  items: PropTypes.array.isRequired,
+  onModifyItem: PropTypes.func.isRequired,
+  onDeleteItem: PropTypes.func.isRequired
+}
+
+// props 默认值设置
+PriceList.defaultProps = {
+  onModifyItem: () => {}
+}
+export default PriceList
